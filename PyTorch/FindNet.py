3f8677cbe1b28f95e2e3518f8b87abd5781d1ff1
Hanna Müller <hanmuell@ethz.ch>
2019-09-19 18:51:27 +0200

    Adds initial version of FindNet (name not def)
    This net should be based on FrontNet, but have an easier quantizable structure
    In the end it should predict in three classes:
    1: no head found
    2: head found
    3: head found and in front (-> taking picture)
    
    But first I'll try to reproduce similar results as the FrontNet, to prove the Net is of any use.

---
+++ PyTorch/FindNet.py
@@ -0,0 +1,86 @@
+
+import torch.nn as nn
+from PreActBlockSimple import PreActBlockSimple
+from nemo.quant.pact_quant import PACT_Conv1d, PACT_Conv2d, PACT_Linear, PACT_Act, PACT_ThresholdAct
+
+
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+
+# FrontNet
+class FindNet(nn.Module):
+    def __init__(self, block, layers, isGray=False):
+        super(FindNet, self).__init__()
+
+        if isGray ==True:
+            self.name = "FindNetGray"
+        else:
+            self.name = "FindNetRGB"
+        self.inplanes = 32
+        self.dilation = 1
+        self._norm_layer = nn.BatchNorm2d
+
+        replace_stride_with_dilation = [False, False, False]
+
+        self.groups = 1
+        self.base_width = 64
+        if isGray == True:
+            self.conv = nn.Conv2d(1, self.inplanes, kernel_size=5, stride=2, padding=2, bias=False)
+        else:
+            self.conv = nn.Conv2d(3, self.inplanes, kernel_size=5, stride=2, padding=2, bias=False)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=5, stride=1, padding=2, bias=False)
+        self.bn32 = nn.BatchNorm2d(32)
+        self.bn64 = nn.BatchNorm2d(64)
+
+        self.relu1 = PACT_Act(statistics_only=True)
+        self.relu = nn.ReLU(inplace=True)
+        #self.relu2 = PACT_Act(statistics_only=True)
+        #self.relu3 = PACT_Act(statistics_only=True)
+        #self.relu4 = PACT_Act(statistics_only=True)
+        #self.relu5 = PACT_Act(statistics_only=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = PreActBlockSimple(32, 32, stride=1)
+        self.layer2 = PreActBlockSimple(32, 64, stride=2)
+        self.layer3 = PreActBlockSimple(64, 128, stride=2)
+
+        self.avg_pool = nn.AvgPool2d(kernel_size=(4, 7), stride=(1, 1))
+        self.fc1 = nn.Linear(128 * block.expansion, 128)
+        self.fc2 = nn.Linear(128, 64)
+
+        self.fc_x = nn.Linear(64, 1)
+        self.fc_y = nn.Linear(64, 1)
+        self.fc_z = nn.Linear(64, 1)
+        self.fc_phi = nn.Linear(64, 1)
+
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn32(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+        #out = self.layer1(out)
+        #out = self.bn32(out)
+        #out = self.relu1(out)
+        out = self.layer2(out)
+        out = self.bn64(out)
+        out = self.relu(out)
+        out = self.layer3(out)
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        x = self.fc_x(out)
+        y = self.fc_y(out)
+        z = self.fc_z(out)
+        phi = self.fc_phi(out)
+
+        return [x, y, z, phi]
+
---
+++ PyTorch/PreActBlockSimple.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+
+class PreActBlockSimple(nn.Module):
+    '''Pre-activation version of the BasicBlock.'''
+    expansion = 1
+
+    # I don't use all these parameters
+    # I just wanted to keep the previous API. I will clean it up once you approve the net
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(PreActBlockSimple, self).__init__()
+
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu2 = nn.ReLU()
+
+
+    def forward(self, x):
+        shortcut = self.shortcut(x) if hasattr(self, 'shortcut') else x
+        out = self.conv1(x)
+        out = self.bn2(out)
+        out = self.relu2(out)
+        out = self.conv2(out)
+        out += shortcut
+        return out
